e7934c071f3737b19e6427dd502013db4b32d2c6
Stefane Fermigier <sf@fermigier.com>
2023-05-02 20:18:44 +0200

    feat: support for multiple config files.

--- src/abilian_devtools/commands/check.py
+++ src/abilian_devtools/commands/check.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023 Abilian SAS <https://abilian.com/>
 #
 # SPDX-License-Identifier: MIT
-
+from pathlib import Path
 from typing import Optional
 
 from cleez.colors import bold
@@ -29,7 +29,12 @@ class CheckCommand(Command):
 
         args_str = " ".join(args)
 
-        run(f"ruff {args_str}")
+        # Todo: more cases
+        if Path("etc").joinpath("ruff.toml").exists():
+            run(f"ruff lint -c etc/ruff.toml {args_str}")
+        else:
+            run(f"ruff {args_str}")
+
         run(f"flake8 {args_str}")
         run(f"mypy --show-error-codes {args_str}")
         run("pyright")
