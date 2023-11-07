279d9fca4916744b0d8462f0d3698f29d5885b19
Stefane Fermigier <sf@fermigier.com>
2023-11-07 17:20:33 +0100

    feat: add `cruft` command.

---
+++ src/abilian_devtools/commands/cruft.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2023 Abilian SAS <https://abilian.com/>
+#
+# SPDX-License-Identifier: MIT
+
+import sys
+from pathlib import Path
+
+from cleez.command import Command
+
+STD_FILES = [
+    ".git/",
+    ".gitignore",
+    "Makefile",
+    "README.md",
+    # noxfile.py (or tox.ini)
+    "poetry.lock",
+    "pyproject.toml",
+    "ruff.toml",
+    "src/",
+    "setup.cfg",
+    "tests/",
+]
+
+
+class CruftCommand(Command):
+    """Run cruft audit."""
+
+    name = "cruft"
+
+    def run(self):
+        self.check_standard_files()
+
+    def check_standard_files(self):
+        """Check standard files ("cruft") are present."""
+
+        for std_file in STD_FILES:
+            if not Path(std_file).exists():
+                print(f"Missing standard file: {std_file}")
+                sys.exit(1)
+
+        if not Path("tox.init").exists() and not Path("noxfile.py").exists():
+            print("Missing tox.ini or noxfile.py")
+            sys.exit(1)
