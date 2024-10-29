a95e2bb491b8b651d5473e415622e88d7180e19b
Stefane Fermigier <sf@abilian.com>
2024-10-29 18:47:48 +0100

    feat: support for both Poetry and uv versionning.

--- src/abilian_devtools/commands/bump.py
+++ src/abilian_devtools/commands/bump.py
@@ -33,42 +33,83 @@ class BumpVersionCommand(Command):
             print("Your repo is dirty. Please commit or stash changes first.")
             sys.exit(1)
 
-        update_version(rule)
-        version = get_version()
+        versionner = Versionner(rule)
+        versionner.update_version()
+        version = versionner.get_version()
+
         run("git add pyproject.toml")
         run(f"git commit -m 'Bump version ({version})'")
         run(f"git tag {version}")
 
 
-def update_version(rule):
-    if rule == "daily":
-        update_version_daily()
-    else:
-        run(f"poetry version {rule}")
+class Versionner:
+    rule: str
+    pyproject: tomlkit.document
 
+    def __init__(self, rule: str):
+        # NB: we're using tomlkit for its roundtrip feature.
+        self.pyproject = tomlkit.parse(Path("pyproject.toml").read_text())
+        self.rule = rule
 
-def update_version_daily():
-    # NB: we're using tomkit for its roundtrip feature.
-    pyproject_d = tomlkit.parse(Path("pyproject.toml").read_text())
+    def read_pyproject(self):
+        self.pyproject = tomlkit.parse(Path("pyproject.toml").read_text())
 
-    version: str = pyproject_d["tool"]["poetry"]["version"]  # type: ignore
-    serial = int(version.split(".")[-1])
-    date = version[: -len(str(serial)) - 1]
+    def write_pyproject(self):
+        Path("pyproject.toml").write_text(tomlkit.dumps(self.pyproject))
 
-    new_date = strftime("%Y.%m.%d", gmtime())
-    if date == new_date:
-        serial += 1
-    else:
-        serial = 1
+    def get_version(self):
+        if "tool" in self.pyproject and "poetry" in self.pyproject["tool"]:
+            version: str = self.pyproject["tool"]["poetry"]["version"]  # type: ignore
+        else:
+            version = self.pyproject["project"]["version"]
 
-    new_version = f"{new_date}.{serial}"
-    print("New version: ", new_version)
-    pyproject_d["tool"]["poetry"]["version"] = new_version  # type: ignore
+        return version
 
-    Path("pyproject.toml").write_text(tomlkit.dumps(pyproject_d))
+    def set_version(self, version: str):
+        if "tool" in self.pyproject and "poetry" in self.pyproject["tool"]:
+            self.pyproject["tool"]["poetry"]["version"] = version
+        else:
+            self.pyproject["project"]["version"] = version
 
+    def update_version(self):
+        if self.rule == "daily":
+            new_version = self.new_version_daily()
+        else:
+            new_version = self.new_version()
 
-def get_version():
-    d = tomlkit.parse(Path("pyproject.toml").read_text())
-    version: str = d["tool"]["poetry"]["version"]  # type: ignore
-    return version
+        print("New version: ", new_version)
+        self.set_version(new_version)
+        self.write_pyproject()
+
+    def new_version_daily(self):
+        version = self.get_version()
+
+        serial = int(version.split(".")[-1])
+        date = version[: -len(str(serial)) - 1]
+
+        new_date = strftime("%Y.%m.%d", gmtime())
+        if date == new_date:
+            serial += 1
+        else:
+            serial = 1
+
+        new_version = f"{new_date}.{serial}"
+        return new_version
+
+    def new_version(self):
+        version = self.get_version()
+        major, minor, patch = version.split(".")
+        match self.rule:
+            case "major":
+                major = str(int(major) + 1)
+                minor = "0"
+                patch = "0"
+            case "minor":
+                minor = str(int(minor) + 1)
+                patch = "0"
+            case "patch":
+                patch = str(int(patch) + 1)
+            case _:
+                raise ValueError(f"Unknown rule: {self.rule}")
+        new_version = f"{major}.{minor}.{patch}"
+        return new_version
